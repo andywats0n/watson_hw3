21b8bdc4a2be84109113713c07e6d8406d4ca955
Andy Watson <andy.watson@ignitionone.com>
2018-09-20 17:06:30 -0400

    added .gitignore and corrected solution to pypoll.py

--- pypoll.py
+++ pypoll.py
@@ -7,21 +7,18 @@ def numFormat(num):
     return "{:,}".format(int(num))
 
 
-def getCandidates(cand):
+def getCounts(cand, counts):
     if(cand not in candidates):
         candidates.append(cand)
-
-
-def getCounts(cand, counts):
-    if(cand == "Marsh"):
+    if(cand == candidates[0]):
         counts[0] += 1
-    if(cand == "Queen"):
+    if(len(candidates) > 1 and cand == candidates[1]):
         counts[1] += 1
-    if(cand == "Bamoo"):
+    if(len(candidates) > 2 and cand == candidates[2]):
         counts[2] += 1
-    if(cand == "Trandee"):
+    if(len(candidates) > 3 and cand == candidates[3]):
         counts[3] += 1
-    if(cand == "Raffah"):
+    if(len(candidates) > 4 and cand == candidates[4]):
         counts[4] += 1
 
 
@@ -50,9 +47,8 @@ with open(filepath, newline="") as file:
 
     for row in reader:
 
-        cand = row[1]
+        cand = row[2]
 
-        getCandidates(cand)
         getCounts(cand, counts)
 
         total += 1
@@ -66,13 +62,16 @@ with open(filepath, newline="") as file:
 
     print(" ")
     print("Election Results")
-    print("-------------------------")
+    print("--------------------------------")
 
     for cand, count, ratio in zip(candidates, counts, ratios):
-        print(f"{cand}:  {ratio}% ({numFormat(count)})")
+        if(len(cand) >= len('o-tooley')):        
+            print(f"{cand}: {ratio}% ({numFormat(count)})")
+        else:
+            print(f"{cand}:\t{ratio}% ({numFormat(count)})")
         winner = getMaxRatio(cand, ratio, maxRatio, winner)
 
-    print(" ")
+    print("--------------------------------")
     print(winner)
 
 with open("results.txt", "w") as results:
@@ -80,13 +79,16 @@ with open("results.txt", "w") as results:
     ratio = 0
     maxRatio = ratios[0]
 
+    print(" ", file=results)
     print("Election Results", file=results)
-    print("-------------------------", file=results)
+    print("--------------------------------", file=results)
 
     for cand, count, ratio in zip(candidates, counts, ratios):
-        print(f"{cand}:  {ratio}% ({numFormat(count)})", file=results)
+        if(len(cand) >= len('o-tooley')):
+            print(f"{cand}: {ratio}% ({numFormat(count)})", file=results)
+        else:
+            print(f"{cand}:\t{ratio}% ({numFormat(count)})", file=results)
         winner = getMaxRatio(cand, ratio, maxRatio, winner)
 
-    print(" ", file=results)
+    print("--------------------------------", file=results)
     print(winner, file=results)
-
